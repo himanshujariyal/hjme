9b19309530abe4f46c13661b146b9ec527c9c51f
mojowebdev <himanshujariyal@gmail.com>
2015-10-07 19:59:59 +0530

    js minify

--- js/main.js
+++ js/main.js
@@ -1,30 +1,29 @@
-	
-	function scrollTo(element, to, duration) {
-	    if (duration < 0) return;
-	    var difference = to - element.scrollTop;
-	    var perTick = difference / duration * 10;
+function scrollTo(element, to, duration) {
+    if (duration < 0) return;
+    var difference = to - element.scrollTop;
+    var perTick = difference / duration * 10;
 
-	    setTimeout(function() {
-	        element.scrollTop = element.scrollTop + perTick;
-	        if (element.scrollTop === to) return;
-	        scrollTo(element, to, duration - 10);
-	    }, 10);
-	}
+    setTimeout(function() {
+        element.scrollTop = element.scrollTop + perTick;
+        if (element.scrollTop === to) return;
+        scrollTo(element, to, duration - 10);
+    }, 10);
+}
 
-	function runScroll() {
-	  scrollTo(document.body, 0, 600);
-	}
+function runScroll() {
+  scrollTo(document.body, 0, 600);
+}
 
-	var scrollme;
-	scrollme = document.querySelector("#scrollTop");
-	scrollme.addEventListener("click",runScroll,false);
+var scrollme;
+scrollme = document.querySelector("#scrollTop");
+scrollme.addEventListener("click",runScroll,false);
 
-	document.getElementById('scrollTop').style.display = 'none';
-    window.onscroll = function(e) {
-      if (document.scrollingElement.scrollTop > window.innerHeight) {
-        document.getElementById('scrollTop').style.display = 'block';
-      }
-      else{
-        document.getElementById('scrollTop').style.display = 'none';
-      }
-	};
+document.getElementById('scrollTop').style.display = 'none';
+window.onscroll = function(e) {
+  if (document.scrollingElement.scrollTop > window.innerHeight) {
+    document.getElementById('scrollTop').style.display = 'block';
+  }
+  else{
+    document.getElementById('scrollTop').style.display = 'none';
+  }
+};
